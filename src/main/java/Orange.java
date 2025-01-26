f9614d0ab43892ae7f6ba6fb245462ff5c0963d8
flyingapricot <neerajkumbar44@gmail.com>
2025-01-26 16:31:49 +0800

    Moved the greeting of the chatbot into its own seperate function

--- src/main/java/Orange.java
+++ src/main/java/Orange.java
@@ -1,11 +1,13 @@
 public class Orange {
     private static final String HORIZONTAL_LINE = "-".repeat(50);
     private static final String CHATBOT_NAME = "ORANGE";
-    public static void main(String[] args) {
+    public void greeting() {
         System.out.println(HORIZONTAL_LINE);
         System.out.println("Hello! I'm " + CHATBOT_NAME);
         System.out.println("What can I do for you?");
         System.out.println(HORIZONTAL_LINE);
+    }
+    public static void main(String[] args) {
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(HORIZONTAL_LINE);
     }
