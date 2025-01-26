afbda24735b4d55f71e5a1f4d6d2266a403ea5b0
flyingapricot <neerajkumbar44@gmail.com>
2025-01-26 19:02:14 +0800

    Reformatted code

--- src/main/java/Orange.java
+++ src/main/java/Orange.java
@@ -2,84 +2,87 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Orange {
-    private static final String HORIZONTAL_LINE = "\t" + "-".repeat(50);
-    private static final String CHATBOT_NAME = "ORANGE";
-    private static ArrayList<Task> tasks = new ArrayList<Task>(); //Array List to store tasks entered by users
+  private static final String HORIZONTAL_LINE = "\t" + "-".repeat(50);
+  private static final String CHATBOT_NAME = "ORANGE";
+  private static ArrayList<Task> tasks =
+      new ArrayList<Task>(); // Array List to store tasks entered by users
 
-    public static void greeting() {
-        System.out.println(HORIZONTAL_LINE);
-        System.out.println("\t" + "Hello! I'm " + CHATBOT_NAME);
-        System.out.println("\t" + "What can I do for you?");
-        System.out.println(HORIZONTAL_LINE);
-    }
+  public static void greeting() {
+    System.out.println(HORIZONTAL_LINE);
+    System.out.println("\t" + "Hello! I'm " + CHATBOT_NAME);
+    System.out.println("\t" + "What can I do for you?");
+    System.out.println(HORIZONTAL_LINE);
+  }
 
-    public static void goodbye() {
-        System.out.println(HORIZONTAL_LINE);
-        System.out.println("\t" + "Bye. Hope to see you again soon!");
-        System.out.println(HORIZONTAL_LINE);
-    }
+  public static void goodbye() {
+    System.out.println(HORIZONTAL_LINE);
+    System.out.println("\t" + "Bye. Hope to see you again soon!");
+    System.out.println(HORIZONTAL_LINE);
+  }
 
-    public static void echo() {
-        String line;
-        Scanner in = new Scanner(System.in);
-        line = in.nextLine();
-        while(!(line.equals("bye"))) {
-            System.out.println(HORIZONTAL_LINE);
-            System.out.println("\t" + line);
-            System.out.println(HORIZONTAL_LINE);
-            line = in.nextLine();
-        }
-        goodbye();
+  public static void echo() {
+    String line;
+    Scanner in = new Scanner(System.in);
+    line = in.nextLine();
+    while (!(line.equals("bye"))) {
+      System.out.println(HORIZONTAL_LINE);
+      System.out.println("\t" + line);
+      System.out.println(HORIZONTAL_LINE);
+      line = in.nextLine();
     }
+    goodbye();
+  }
 
-    public static void ListTasks() {
-        System.out.println(HORIZONTAL_LINE);
-        int count = 1;
-        for(int i =0;i<tasks.size();i++) {
-            System.out.println("\t" + count + ". " + tasks.get(i).GetTaskWithCompletion());
-            count++;
-        }
-        System.out.println(HORIZONTAL_LINE);
+  public static void ListTasks() {
+    System.out.println(HORIZONTAL_LINE);
+    int count = 1;
+    for (int i = 0; i < tasks.size(); i++) {
+      System.out.println("\t" + count + ". " + tasks.get(i).GetTaskWithCompletion());
+      count++;
     }
+    System.out.println(HORIZONTAL_LINE);
+  }
 
-    public static void AddTask(String task) {
-        System.out.println(HORIZONTAL_LINE);
-        System.out.println("\t" + "added: " + task);
-        Task t = new Task(task,false);
-        tasks.add(t);
-        System.out.println(HORIZONTAL_LINE);
-    }
+  public static void AddTask(String task) {
+    System.out.println(HORIZONTAL_LINE);
+    System.out.println("\t" + "added: " + task);
+    Task t = new Task(task, false);
+    tasks.add(t);
+    System.out.println(HORIZONTAL_LINE);
+  }
 
-    public static void main(String[] args) {
-        greeting(); //Print out the greeting
-        String line;
-        Scanner in = new Scanner(System.in);
-        line = in.nextLine();
-        while(!(line.equals("bye"))) {
-            if(line.equals("list")) {
-                ListTasks();
-            }else if(line.contains("unmark")) {
-                //Mark the task as not completed
-                String taskNumber = line.substring(7);
-                tasks.get(Integer.parseInt(taskNumber)-1).setIsDone(false);
-                System.out.println(HORIZONTAL_LINE);
-                System.out.println("\t" + "OK, I've marked this task as not done yet:");
-                System.out.println("\t\t" + tasks.get(Integer.parseInt(taskNumber)-1).GetTaskWithCompletion());
-                System.out.println(HORIZONTAL_LINE);
-            }else if(line.contains("mark")) {
-                //Mark the task as completed
-                String taskNumber = line.substring(5);
-                tasks.get(Integer.parseInt(taskNumber)-1).setIsDone(true);
-                System.out.println(HORIZONTAL_LINE);
-                System.out.println("\t" + "Nice! I've marked this task as done:");
-                System.out.println("\t\t" + tasks.get(Integer.parseInt(taskNumber)-1).GetTaskWithCompletion());
-                System.out.println(HORIZONTAL_LINE);
-            }else {
-                //Add the task to the tasks list
-                AddTask(line);
-            }
-            line = in.nextLine();
-        }
-        goodbye();
+  public static void main(String[] args) {
+    greeting(); // Print out the greeting
+    String line;
+    Scanner in = new Scanner(System.in);
+    line = in.nextLine();
+    while (!(line.equals("bye"))) {
+      if (line.equals("list")) {
+        ListTasks();
+      } else if (line.contains("unmark")) {
+        // Mark the task as not completed
+        String taskNumber = line.substring(7);
+        tasks.get(Integer.parseInt(taskNumber) - 1).setIsDone(false);
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("\t" + "OK, I've marked this task as not done yet:");
+        System.out.println(
+            "\t\t" + tasks.get(Integer.parseInt(taskNumber) - 1).GetTaskWithCompletion());
+        System.out.println(HORIZONTAL_LINE);
+      } else if (line.contains("mark")) {
+        // Mark the task as completed
+        String taskNumber = line.substring(5);
+        tasks.get(Integer.parseInt(taskNumber) - 1).setIsDone(true);
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("\t" + "Nice! I've marked this task as done:");
+        System.out.println(
+            "\t\t" + tasks.get(Integer.parseInt(taskNumber) - 1).GetTaskWithCompletion());
+        System.out.println(HORIZONTAL_LINE);
+      } else {
+        // Add the task to the tasks list
+        AddTask(line);
+      }
+      line = in.nextLine();
     }
+    goodbye();
+  }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,47 +1,47 @@
 public class Task {
-    protected String description;
-    protected boolean isDone;
+  protected String description;
+  protected boolean isDone;
 
-    public Task(String description,boolean isDone) {
-        this.description = description;
-        this.isDone = isDone;
-    }
+  public Task(String description, boolean isDone) {
+    this.description = description;
+    this.isDone = isDone;
+  }
 
-    public Task(String description) {
-        this.description = description;
-        this.isDone = false;
-    }
+  public Task(String description) {
+    this.description = description;
+    this.isDone = false;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public String getDescription() {
+    return description;
+  }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  public void setDescription(String description) {
+    this.description = description;
+  }
 
-    public boolean getIsDone() {
-        return isDone;
-    }
+  public boolean getIsDone() {
+    return isDone;
+  }
 
-    public void printTaskWithCompletion() {
-        if(this.isDone == true) {
-            System.out.print("[X] " + this.description);
-        }else if(this.isDone == false) {
-            System.out.print("[ ] " + this.description);
-        }
-    }
+  public void setIsDone(boolean isDone) {
+    this.isDone = isDone;
+  }
 
-    public String GetTaskWithCompletion() {
-        if(this.isDone == true) {
-            return "[X] " + this.description;
-        }else if(this.isDone == false) {
-            return "[ ] " + this.description;
-        }
-        return "";
+  public void printTaskWithCompletion() {
+    if (this.isDone) {
+      System.out.print("[X] " + this.description);
+    } else if (!this.isDone) {
+      System.out.print("[ ] " + this.description);
     }
+  }
 
-    public void setIsDone(boolean isDone) {
-        this.isDone = isDone;
+  public String GetTaskWithCompletion() {
+    if (this.isDone) {
+      return "[X] " + this.description;
+    } else if (!this.isDone) {
+      return "[ ] " + this.description;
     }
+    return "";
+  }
 }
