3031c1d5ee92818b85432442929307f20bdd6a8c
flyingapricot <neerajkumbar44@gmail.com>
2025-01-26 16:41:16 +0800

    Added goodbye function to display goodbye and added echo function to echo what user says

--- src/main/java/Orange.java
+++ src/main/java/Orange.java
@@ -1,14 +1,37 @@
+import java.util.Scanner;
+
 public class Orange {
-    private static final String HORIZONTAL_LINE = "-".repeat(50);
+    private static final String HORIZONTAL_LINE = "\t" + "-".repeat(50);
     private static final String CHATBOT_NAME = "ORANGE";
     public static void greeting() {
         System.out.println(HORIZONTAL_LINE);
-        System.out.println("Hello! I'm " + CHATBOT_NAME);
-        System.out.println("What can I do for you?");
+        System.out.println("\t" + "Hello! I'm " + CHATBOT_NAME);
+        System.out.println("\t" + "What can I do for you?");
         System.out.println(HORIZONTAL_LINE);
     }
-    public static void main(String[] args) {
-        System.out.println("Bye. Hope to see you again soon!");
+
+    public static void goodbye() {
         System.out.println(HORIZONTAL_LINE);
+        System.out.println("\t" + "Bye. Hope to see you again soon!");
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    public static void echo() {
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while(!(line.equals("bye"))) {
+            System.out.println(HORIZONTAL_LINE);
+            System.out.println("\t" + line);
+            System.out.println(HORIZONTAL_LINE);
+            in = new Scanner(System.in);
+            line = in.nextLine();
+        }
+        goodbye();
+    }
+
+    public static void main(String[] args) {
+        greeting(); //Print out the greeting
+        echo(); //Echo user's input
     }
 }
