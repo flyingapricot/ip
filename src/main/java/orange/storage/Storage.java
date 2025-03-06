069fc80279ef720324306e8a7621ffb438bbe2bf
flyingapricot <126645544+flyingapricot@users.noreply.github.com>
2025-03-06 15:31:53 +0800

    Update Storage.java

--- src/main/java/orange/storage/Storage.java
+++ src/main/java/orange/storage/Storage.java
@@ -79,28 +79,28 @@ public class Storage {
                 String finalTask = "";
                 switch (task.getClass().getName()) {
                 case "orange.task.Todo":
-finalTask = "T," + task.getIsDone() + "," + task.getDescription() + ",-,-";
-break;
+                finalTask = "T," + task.getIsDone() + "," + task.getDescription() + ",-,-";
+                break;
                 case "orange.task.Deadline":
-finalTask =
-"D,"
-+ task.getIsDone()
-+ ","
-+ task.getDescription()
-+ ",-,"
-+ ((Deadline) task).getDateAndTime();
-break;
+                finalTask =
+                "D,"
+                + task.getIsDone()
+                + ","
+                + task.getDescription()
+                + ",-,"
+                + ((Deadline) task).getDateAndTime();
+                break;
                 case "orange.task.Events":
-finalTask =
-"E,"
-+ task.getIsDone()
-+ ","
-+ task.getDescription()
-+ ","
-+ ((Events) task).getStartDateAndTime()
-+ ","
-+ ((Events) task).getEndDateAndTime();
-break;
+                finalTask =
+                "E,"
+                + task.getIsDone()
+                + ","
+                + task.getDescription()
+                + ","
+                + ((Events) task).getStartDateAndTime()
+                + ","
+                + ((Events) task).getEndDateAndTime();
+                break;
                 default:
                 }
                 Files.write(path, List.of(finalTask), StandardOpenOption.APPEND);
@@ -124,26 +124,26 @@ break;
                 String[] values = line.split(",");
                 switch (values[0]) {
                 case "T":
-TaskList.getInstance()
-.addTask(new Todo(values[2], Boolean.parseBoolean(values[1])));
-break;
+                TaskList.getInstance()
+                .addTask(new Todo(values[2], Boolean.parseBoolean(values[1])));
+                break;
                 case "D":
-TaskList.getInstance()
-.addTask(
-new Deadline(
-values[2],
-Boolean.parseBoolean(values[1]),
-values[4]));
-break;
+                TaskList.getInstance()
+                .addTask(
+                new Deadline(
+                values[2],
+                Boolean.parseBoolean(values[1]),
+                values[4]));
+                break;
                 case "E":
-TaskList.getInstance()
-.addTask(
-new Events(
-values[2],
-Boolean.parseBoolean(values[1]),
-values[4],
-values[3]));
-break;
+                TaskList.getInstance()
+                .addTask(
+                new Events(
+                values[2],
+                Boolean.parseBoolean(values[1]),
+                values[4],
+                values[3]));
+                break;
                 }
             }
         } catch (IOException e) {
